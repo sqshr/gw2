10f5ee5ef1e4cdd8358a90358a825f6a9916f8ec
sqshr <mike@memery.info>
2022-10-21 09:34:17 +0100

    logic flaw and refactoring. Debug output added

--- gw2.py
+++ gw2.py
@@ -7,11 +7,16 @@ import scapy.all as scapy
 parser = argparse.ArgumentParser(description='This tool tries to find gateways present on a local LAN')
 parser.add_argument('-i', '--ip', default='8.8.8.8', help='The IP address to try and reach')
 parser.add_argument('-I', '--interface', default='eth0', help='The network interface to use')
+parser.add_argument('-d', '--debug', default=False, action='store_true',  help='Debug output')
 parser.add_argument('ips', help='File containing IP addresses to try to use as a gateway')
 args = parser.parse_args()
 
 #Makes scapy quiet
 scapy.conf.verb = 0
+
+#Sets debug output
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 #Gets source IP from selected interface
 source_ip = netifaces.ifaddresses(args.interface)[netifaces.AF_INET][0]['addr']
@@ -38,10 +43,26 @@ for ip in iplist:
 #Array of routing IPs:
 routers=[]
 
-def isrouter(response):
-    if response.haslayer("ICMP") and synack_reply["ICMP"].type == 11 and synack_reply["ICMP"].code == 0:
+#checks for a TTL exceeded response- in which case it's a router
+def gotresponse(response):
+    if response.haslayer("ICMP") and response["ICMP"].type == 11 and response["ICMP"].code == 0:
         if router_ip not in routers:
             routers.append(router_ip)
+    for p in response:
+        a = p.show(dump=True)
+        logging.debug(type(a))
+        logging.debug(a)
+
+def sendpacket(reqtype,packet):
+    response = ''
+    logging.debug(f"Sending "+reqtype+" via "+router_ip)
+    response = scapy.srp1(packet, timeout=0.2 )
+    if response:
+        gotresponse(response)
+    else:
+        logging.debug(f"No response received")
+
+    
 
 for ip in ipdict:
     router_ip = ip
@@ -53,23 +74,12 @@ for ip in ipdict:
     #Send SYN packet to port 80
     ip = scapy.IP( src=source_ip , dst=args.ip, ttl=1 )
     syn_packet = scapy.TCP(sport=1500, dport=80, flags="S" )
-    synack_reply = scapy.srp1(ether/ip/syn_packet, timeout=0.2 )
-    if synack_reply:
-        #for p in synack_reply:
-        #    a = p.show(dump=True)
-        #    print(type(a))
-        #    print(a)
-        isrouter(synack_reply)
+    packet = ether/ip/syn_packet
+    sendpacket("SYN",packet)
 
     #send ICMP ping
     packet = ether/scapy.IP(dst=args.ip, ttl=1)/scapy.ICMP()
-    ping_reply = scapy.srp1(packet,timeout=0.2)
-    if ping_reply:
-        #for p in ping_reply:
-        #    a = p.show(dump=True)
-        #    #print(type(a))
-        #    print(a)
-        isrouter(ping_reply)
+    sendpacket("ICMP",packet)
 
 #Prints out any routers found
 for ip in routers:
